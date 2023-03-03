77fda7a2467f5ca1e868f08423d9649a09084a86
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-03-03 19:27:01 +0700

    GraphQL endpoint of products

--- src/app.py
+++ src/app.py
@@ -6,5 +6,9 @@ from schemes import product_schema, convert_execution_result_to_dict
 app = Flask(__name__)
 
 
+@app.route("/api/products", methods=('POST', ))
+def product_endpoint():
+    return jsonify(convert_execution_result_to_dict(product_schema.execute(request.json["query"])))
+
 if __name__ == "__main__":
     app.run(port='8000')
